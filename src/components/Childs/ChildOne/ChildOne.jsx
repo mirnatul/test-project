ac40fdfe831c6ab4d925493dd518630656c07323
mirnatul <www.mirnatul@gmail.com>
2025-06-22 19:49:27 +0600

    state lifting from child to parent completed

--- src/components/Childs/ChildOne/ChildOne.jsx
+++ src/components/Childs/ChildOne/ChildOne.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const ChildOne = () => {
+const ChildOne = ({ singleCountry, handleVisited }) => {
+    if (!singleCountry) return <div>Loading...</div>;
+    const { name } = singleCountry
+
     return (
-        <div className='border-2 p-2 mb-2 bg-amber-200'>
-            Child Container 1
+        <div className=''>
+            <div className='border-2 p-2 mb-2 bg-amber-200 flex justify-between'>
+                <div>{name.common}</div>
+                <button
+                    onClick={() => handleVisited(singleCountry)}
+                    className='bg-sky-400 hover:bg-sky-200 px-2 py-1 rounded-md'>Visited</button>
+            </div>
         </div>
     );
 };
--- src/components/Parent/Parent.jsx
+++ src/components/Parent/Parent.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ChildOne from '../Childs/ChildOne/ChildOne';
 import ChildTwo from '../Childs/ChildTwo/ChildTwo';
 
 const Parent = () => {
+    const [country, setCountry] = useState([])
+    const [visited, setVisited] = useState([])
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/independent?status=true')
+            .then(res => res.json())
+            .then(data => setCountry(data))
+    }, [])
+
+    const handleVisited = country => {
+        // console.log(country);
+        const newVisited = [...visited, country];
+        setVisited(newVisited);
+        console.log("Clicked");
+    }
+
+    // console.log(country);
     return (
         <div className='border-4 p-4 bg-red-200'>
-            <div>
-                Parent Container
+            <div className='mb-4 sticky top-0 bg-red-200'>
+                <p>Total country: <b>{country.length}</b></p>
+                <p>Visited: <b>{visited.length}</b></p>
+            </div>
+            <div className='grid grid-cols-3 gap-2'>
+                {
+                    country.map(singleCountry => <ChildOne
+                        key={singleCountry?.cca2}
+                        singleCountry={singleCountry}
+                        handleVisited={handleVisited}
+                    ></ChildOne>)
+                }
             </div>
             <ChildOne></ChildOne>
-            <ChildTwo></ChildTwo>
+            {/* <ChildTwo></ChildTwo> */}
         </div>
     );
 };
